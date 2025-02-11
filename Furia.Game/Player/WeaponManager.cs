5d711a43955bf63a9a93dca5ad73a569747dfd63
Kuvrot <jamessanz@outlook.com>
2025-02-11 00:43:01 -0600

    flashlight implemented

---
+++ Furia.Game/Player/WeaponManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stride.Core.Mathematics;
+using Stride.Input;
+using Stride.Engine;
+
+namespace Furia.Player
+{
+    public class WeaponManager : SyncScript
+    {
+        public bool enableFlashLight = true;
+
+        public LightComponent flashLight;
+
+        public override void Start()
+        {
+            // Initialization of the script.
+        }
+
+        public override void Update()
+        {
+            if (enableFlashLight)
+            {
+                if (flashLight != null)
+                {
+                    if (Input.IsKeyPressed(Keys.F) && flashLight.Enabled)
+                    {
+                        flashLight.Enabled = false;
+                    }
+                    else if (Input.IsKeyPressed(Keys.F) && !flashLight.Enabled)
+                    {
+                        flashLight.Enabled = true;
+                    }
+                }
+            }
+        }
+    }
+}
